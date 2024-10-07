725dd111f2f04d06c50fefa2eaca38ccce5d3ea1
maulidihidayat <ahmadhapinuddin@gmail.com>
2024-10-07 17:09:59 +0800

    dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <>
+      <div className="">
+
+        <h4>Dashboard</h4>
+        <p>7 oktober 2024</p>
+
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
