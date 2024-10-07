3fbdd4a7aa35566084cc9508c1069fe7f54fcf1d
maulidihidayat <maulidi9999@gmail.com>
2024-10-07 22:00:51 +0700

    resolve cnflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,39 +2,40 @@ import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 const config: Config = withMT({
-    darkMode: ["class"],
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-        './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
-    ],
-    theme: {
-        extend: {
-            colors: {
-                customBlue: '#0065DB',
-                customPink: '#EA1565',
-                customYellow: '#FDE86F',
-                customGreen: '#65DB00',
-                customDarkBlue: '#005AAB',
-                customDarkenBlue: '#26136A',
-                background: 'hsl(var(--background))',
-                foreground: 'hsl(var(--foreground))',
-                card: {
-                    DEFAULT: 'hsl(var(--card))',
-                    foreground: 'hsl(var(--card-foreground))'
-                },
-                // ...other color configurations
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)'
-            }
-        }
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#0065DB',
+        customPink: '#EA1565',
+        customYellow: '#FDE86F',
+        customGreen: '#65DB00',
+        customDarkBlue: '#005AAB',
+        customDarkenBlue: '#26136A',
+        border: '#D1D5DB', // Example of a custom border color
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        // Add more colors as needed
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
     },
-    plugins: [require("tailwindcss-animate")],
+  },
+  plugins: [require("tailwindcss-animate")],
 });
 
 export default config;
