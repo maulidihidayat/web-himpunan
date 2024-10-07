65d1a569fda498e5eb9e3c47910f4483fde205eb
maulidihidayat <maulidi9999@gmail.com>
2024-10-07 21:48:09 +0700

    perbaikan conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,83 +1,40 @@
 import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-<<<<<<< HEAD
-const config: Config = {
+const config: Config = withMT({
     darkMode: ["class"],
     content: [
-=======
-const config: Config = withMT({
-  darkMode: ["class"],
-  content: [
->>>>>>> 272788920100b3dc3f6911e11329638dcf8970ce
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			customBlue: '#0065DB',
-  			customPink: '#EA1565',
-  			customYellow: '#FDE86F',
-  			customGreen: '#65DB00',
-  			customDarkBlue: '#005AAB',
-  			customDarkenBlue: '#26136A',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-<<<<<<< HEAD
-};
-=======
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                customBlue: '#0065DB',
+                customPink: '#EA1565',
+                customYellow: '#FDE86F',
+                customGreen: '#65DB00',
+                customDarkBlue: '#005AAB',
+                customDarkenBlue: '#26136A',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                },
+                // ...other color configurations
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            }
+        }
+    },
+    plugins: [require("tailwindcss-animate")],
 });
 
->>>>>>> 272788920100b3dc3f6911e11329638dcf8970ce
 export default config;
