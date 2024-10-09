a6f0c4a55812510b6bbd1e311ce007977c32a30a
maulidihidayat <maulidi9999@gmail.com>
2024-10-09 23:27:37 +0700

    Landing page done

--- src/Image/image.ts
+++ src/Image/image.ts
@@ -3,7 +3,7 @@ import gambar1 from './widgets/gambar1.png'
 import gambar2 from './widgets/gambar2.png'
 import gambar3 from './widgets/gamabr4.png'
 import logo from './widgets/logo.png'
-import logo2 from './widgets/logopif.jpg'
+import logo2 from './widgets/logopif.png'
 
 
 const image = {
---
+++ src/components/Members/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+import image from "@/Image/image";
+
+const index = () => {
+  return (
+    <div>
+      <Image
+        src={image.gambar2}
+        alt="My cool image"
+        width={3416}
+        height={1282.6}
+      />
+    </div>
+  );
+};
+
+export default index;
