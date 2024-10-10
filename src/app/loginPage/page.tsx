14958e48c71958cf461fb0975d831046cb5aab9b
maulidihidayat <ahmadhapinuddin@gmail.com>
2024-10-10 16:53:16 +0800

    loginPage,register,reset

--- src/app/loginPage/page.tsx
+++ src/app/loginPage/page.tsx
@@ -1,22 +1,76 @@
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
-import LoginPage from '@/components/LoginPage'
-<<<<<<< HEAD
-import logo from "@/Image/widgets/logopif.png"
-import gambar from "@/Image/widgets/Group 6.jpg"
-=======
 import logo from "@/Assets/Image/widgets/logopif.png"
-import gambar from "@/Assets/Image/widgets/Group 6.jpg"
-import Background from '@/components/Background'
->>>>>>> 7baafdc336f44c3ac59486e364d5e444096b62f6
+import Link from 'next/link'
+import { Link2, SquareCheck } from 'lucide-react'
 
 
-export default function page() {
+
+const LoginPage = () => {
   return (
-    
+
+<div className=''>
+
+    <div className='fixed bg-white h-[100vh] w-[470px] right-0'>
+
+        <div className="flex justify-center mt-12">
+        <Image
+            src={logo}
+            width={92}
+            height={90}
+            alt=""
+            className="pt-10"
+        />
+        </div>
+
+        <div className="flex justify-center">
+            <form action="">
+
+                {/* Input email */}
+                <div className='m-4 '>
+                <label htmlFor="email" className='block text-black font-semibold mb-2'>Email</label>
+                <input type="email" id="email" name="email" placeholder='ahmadhapinuddin@gmail.com' className='w-96 h-14 border border-gray-500 rounded-xl px-4 placeholder:text-center' />
+                </div>
+                {/* Email end */}
+
+                {/* Input password */}
+                <div className='m-4'>
+                    <label htmlFor="password"  className='block text-black font-semibold mb-2'>Password</label>
+                    <input type="password" id='password' name='password' placeholder='******' className='w-96 h-14 border border-gray-500 rounded-xl px-4 placeholder:text-center' />
+                </div>
+                {/* Password end */}
+
+                <div className='flex justify-center'>
+                <SquareCheck
+                className='pr-1'/>
+                <Link className="text-black text-[10px] pt-1" href={"/"}>I Agree to the terms of service and privacy public </Link>
+                {/* <p className='text-black text-[10px] pt-1'>I Agree to the terms of service and privacy public</p> */}
+                </div>
+
+            </form>
+        </div>
+
+        <div className=" flex justify-center">
+          <button className="bg-customPink hover:bg-pink-300 font-semibold text-white p-1 mt-2 rounded-xl w-96 h-14">Login</button>
+        </div>
+        <Link className="flex justify-evenly font-semibold text-[10px] m-2 text-customPink" href={"/"}>Forgot Password?</Link>
+
+        <span className="text-black font-semibold flex justify-center">Or</span>
+
         <div>
-        <LoginPage logo={logo} />
+          
+        </div>
+        <span className="text-black flex justify-center text-sm">Already have an account?<Link className="flex justify-center text-sm text-customPink space-x-5" href={"/"}>Sign in</Link></span>
+
+        <div>
         
         </div>
-        
+
+    </div>
+
+</div>  
+    
   )
 }
+
+export default LoginPage
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,15 +1,59 @@
-import Register from '@/components/Register'
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
-import logo from "@/Image/widgets/logopif.png"
+import logo from "@/Assets/Image/widgets/logopif.png"
+import Link from 'next/link'
 
-
-
-export default function page() {
+const Register = () =>{
   return (
-    
-        <div>
-            <Register logo={logo} />
+    <div className='fixed bg-white h-[100vh] w-[470px] right-0'>
+
+        <div className="flex justify-center mt-12">
+        <Image
+            src={logo}
+            width={92}
+            height={90}
+            alt=""
+            className="pt-10"
+        />
         </div>
+
+        <div className="flex justify-center">
+            <form action="">
+
+                {/* Name Input */}
+                <div className="m-4">
+                  <label htmlFor="fname" className="block text-black font-semibold mb-2">Name</label>
+                  <input type="text" id="fname" name="lname" placeholder='ahmadhapinuddin' className="w-96 h-14 border border-gray-500 rounded-xl px-4 placeholder:text-center" />
+                </div>
+
+                {/* Input email */}
+                <div className='m-4 '>
+                <label htmlFor="email" className='block text-black font-semibold mb-2'>Email</label>
+                <input type="email" id="email" name="email" placeholder='ahmadhapinuddin@gmail.com' className='w-96 h-14 border border-gray-500 rounded-xl px-4 placeholder:text-center' />
+                </div>
+                {/* Email end */}
+
+                {/* Input password */}
+                <div className='m-4'>
+                    <label htmlFor="password" className='block text-black font-semibold mb-2'>Password</label>
+                    <input type="password" id='password' name='password' placeholder='******' className='w-96 h-14 border border-gray-500 rounded-xl px-4 placeholder:text-center' />
+                </div>
+                {/* Password end */}
+
+            </form>
+        </div>
+
+        <div className=" flex justify-center">
+            <button className="bg-customPink hover:bg-pink-300 font-semibold text-white p-1 mt-2 rounded-xl w-96 h-14">Login</button>
+        </div>
+        <Link className="flex justify-evenly font-semibold text-[10px] m-2 text-customPink" href={"/"}>Forgot Password?</Link>
+
+        <div>
         
+        </div>
+
+    </div>
   )
 }
+
+export default Register
