f6db0b3a4312c6ec1c2f3de6660ad8cebced2985
maulidihidayat <maulidi9999@gmail.com>
2024-10-15 20:05:17 +0700

    fitur settings selesai

--- src/app/Home/Settings/page.tsx
+++ src/app/Home/Settings/page.tsx
@@ -1,9 +1,7 @@
 import HeaderDashboard from "@/components/Headerdashboard";
 import SideBar from "@/components/SideBar";
-import { Card, CardBody } from "@material-tailwind/react";
 import React from "react";
 import Setting from "./Setting";
-import Form from "./form";
 
 export default function Page() {
   return (
