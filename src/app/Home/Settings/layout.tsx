849a65a7959798b09b84006beb6b16c369a384c3
maulidihidayat <maulidi9999@gmail.com>
2024-10-26 22:35:49 +0700

    commit aja

--- src/app/Home/Settings/layout.tsx
+++ src/app/Home/Settings/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <h1>hallo dunia</h1>
         {children}
     </div>
   );
