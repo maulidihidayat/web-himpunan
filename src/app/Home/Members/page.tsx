f3d6ce62e6e1f43981a2d57358b3e60b2a915a31
maulidihidayat <maulidi9999@gmail.com>
2024-11-05 11:17:40 +0700

    form validatio

--- src/app/Home/Members/page.tsx
+++ src/app/Home/Members/page.tsx
@@ -1,10 +1,36 @@
 "use client";
 import InputForm from "@/components/InputForm";
+import { Fullscreen } from "lucide-react";
 import React from "react";
 import { FieldValues, FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z  } from "zod";
 
+const MemberSchema = z.object({
+  nim: z.string().min(10).max(15),
+  email: z.string().email(),
+  name: z.string().min(3, { message: "Nama tidak boleh kurang dari 3 huruf" }),
+  phone_number: z.string().min(12),
+  address: z.string().min(5),
+  gender: z.string(),
+  active: z.number(),
+  brithdate: z.string().min(1),
+});
 export default function Page() {
-  const methods = useForm();
+  const methods = useForm<z.infer<typeof MemberSchema>>({
+    defaultValues: {
+      nim: "",
+      email: "",
+      name: "",
+      phone_number: "",
+      address: "",
+      gender: "Laki-Laki",
+      active: 1,
+      brithdate: "",
+    },
+    mode: "all",
+    resolver: zodResolver(MemberSchema),
+  });
   const { handleSubmit } = methods;
 
   const handleSave = (data: FieldValues) => {
@@ -28,6 +54,7 @@ export default function Page() {
                 label="Full Name"
                 name="name"
                 type="text"
+                required={false}
               />
 
               <InputForm
@@ -35,18 +62,21 @@ export default function Page() {
                 label="Nomor Induk Mahasiswa"
                 type="text"
                 name="nim"
+                required={false}
               />
               <InputForm
                 placeholder="udin@gmail.com"
                 label="Email"
                 name="email"
                 type="email"
+                required={false}
               />
               <InputForm
                 placeholder="087453XXXX"
                 label="Phone Number"
                 name="phone_number"
                 type="tel"
+                required={false}
               />
             </div>
             <div className="space-y-4">
@@ -55,12 +85,14 @@ export default function Page() {
                 label="Alamat"
                 name="address"
                 type="text"
+                required={false}
               />
               <InputForm
                 placeholder=""
                 label="Tanggal Lahir"
                 name="birthdate"
                 type="date"
+                required={false}
               />
               <InputForm
                 label="Jenis Kelamin"
@@ -71,6 +103,7 @@ export default function Page() {
                   { value: "Perempuan", label: "Perempuan" },
                 ]}
                 placeholder={""}
+                required={false}
               />
 
               <InputForm
@@ -79,9 +112,11 @@ export default function Page() {
                 placeholder={""}
                 type="radio"
                 options={[
-                  { value: "Aktif", label: "Aktif" },
-                  { value: "Tidak Aktif", label: "Tidak Aktif" },
+                  { value: 1, label: "Aktif" },
+                  { value: 0, label: "Tidak Aktif" },
                 ]}
+                isNumber
+                required={false}
               />
             </div>
             <div className="col-span-2 flex justify-center mt-6">
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { log } from "console";
 
 type InputFormProps = {
   name: string;
@@ -7,7 +9,8 @@ type InputFormProps = {
   type: string;
   placeholder: string;
   required: boolean;
-  options?: { value: string; label: string }[];
+  options?: { value: string | number; label: string }[];
+  isNumber?: boolean;
 };
 
 export default function InputForm({
@@ -16,44 +19,73 @@ export default function InputForm({
   type,
   placeholder,
   options,
-  required
+  required,
+  isNumber,
 }: InputFormProps) {
-  const { register } = useFormContext();
-
+  const {
+    register,
+    control,
+    formState: { errors },
+  } = useFormContext();
   return (
     <div className="w-[335px]">
       <label className="block text-customgray font-bold mt-2">{label}</label>
-      {type === 'radio' && options ? (
+      {type === "radio" && options ? (
         <div className="flex space-x-4 mt-2">
-          {options.map((option) => (
-            <label key={option.value} className="flex items-center space-x-2">
-              <input
-                {...register(name)}
-                type="radio"
-                value={option.value}
-                className="form-radio"
-              />
-              <span>{option.label}</span>
-            </label>
+          {options.map((data, index) => (
+            <Controller
+              key={index}
+              control={control}
+              name={name}
+              render={({ field: { value: val, onChange } }) => {
+                return (
+                  <label className="flex items-center space-x-2">
+                    <input
+                      checked={data.value === val}
+                      value={data.value}
+                      type="radio"
+                      className="form-radio"
+                      onChange={(value) =>
+                        onChange(
+                          isNumber
+                            ? Number(value.target.value)
+                            : value.target.value
+                        )
+                      }
+                    />
+                    <span>{data.label}</span>
+                  </label>
+                );
+              }}
+            />
           ))}
         </div>
-      ) : type === 'textarea' ? (
+      ) : type === "textarea" ? (
         <textarea
           {...register(name)}
           placeholder={placeholder}
           required={required}
           className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-customPink"
-          rows={4} 
+          rows={4}
         />
       ) : (
         <input
-          {...register(name)}
+          {...register(name, {
+            setValueAs: (value) => (isNumber ? Number(value) : value),
+          })}
           type={type}
           placeholder={placeholder}
           required={required}
           className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-customPink"
         />
       )}
+      <ErrorMessage
+        errors={errors}
+        name={name}
+        render={({ message }) => (
+          <p className="text-sm text-customPink mt-1 p-1">{message}</p>
+        )}
+      />
     </div>
   );
 }
